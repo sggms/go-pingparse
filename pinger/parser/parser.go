79fd0f1e496c83dcd5b191997a0df92c7edf73b0
gm42 <gm42@users.noreply.github.com>
2017-08-29 10:55:53 +0200

    Bulk of the parsing logic implemented

---
+++ pinger/parser/parser.go
@@ -0,0 +1,170 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	ErrNotEnoughLines        = errors.New("not enough lines")
+	ErrHeaderMismatch        = errors.New("header mismatch")
+	ErrUnexpectedFloatFormat = errors.New("unexpected float format")
+	ErrUnrecognizedLine      = errors.New("unrecognized ping reply line")
+	ErrMalformedStats        = errors.New("malformed stats")
+)
+
+var (
+	headerRx         = regexp.MustCompile(`^PING (\d+\.\d+\.\d+\.\d+) \((\d+\.\d+\.\d+\.\d+)\) (\d+)\((\d+)\) bytes of data\.$`)
+	lineRx           = regexp.MustCompile(`^(\d+) bytes from (\d+\.\d+\.\d+\.\d+): icmp_seq=(\d+) ttl=(\d+) time=(\d+\.\d+) ms$`)
+	statsSeparatorRx = regexp.MustCompile(`^--- (\d+\.\d+\.\d+\.\d+) ping statistics ---$`)
+	statsLine1       = regexp.MustCompile(`^(\d+) packets transmitted, (\d+) received, (\d+)% packet loss, time (\d+)ms$`)
+	statsLine2       = regexp.MustCompile(`^rtt min/avg/max/mdev = (\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+) ms$`)
+)
+
+// PingOutput contains the whole ping operation output.
+type PingOutput struct {
+	IPAddress         string
+	ResolvedIPAddress string
+	PayloadSize       uint
+	PayloadActualSize uint
+	Replies           []PingReply
+	Stats             PingStatistics
+}
+
+// PingReply contains an individual ping reply line.
+type PingReply struct {
+	Size           uint
+	FromAddress    string
+	SequenceNumber uint
+	TTL            uint
+	Time           time.Duration
+}
+
+// PingStatistics contains the statistics of the whole ping operation.
+type PingStatistics struct {
+	IPAddress          string
+	PacketsTransmitted uint
+	PacketsReceived    uint
+	PacketLossPercent  uint8
+	Time               time.Duration
+	RoundTripTime      time.Duration
+	AverageTime        time.Duration
+	Max                time.Duration
+	MeanDeviation      time.Duration
+}
+
+func Parse(s string) (*PingOutput, error) {
+	var po PingOutput
+
+	// separate full output text into lines
+	lines := strings.Split(s, "\n")
+	if len(lines) < 5 {
+		return nil, ErrNotEnoughLines
+	}
+
+	m := headerRx.FindStringSubmatch(lines[0])
+	if len(m) < 5 {
+		return nil, ErrHeaderMismatch
+	}
+	po.IPAddress = m[1]
+	po.ResolvedIPAddress = m[2]
+	payloadSize, err := strconv.ParseUint(m[3], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.PayloadSize = uint(payloadSize)
+	payloadActualSize, err := strconv.ParseUint(m[4], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.PayloadActualSize = uint(payloadActualSize)
+
+	var last int
+	for i, line := range lines[1:] {
+		if line == "" {
+			last = i
+			break
+		}
+		m := lineRx.FindStringSubmatch(line)
+		if len(m) < 6 {
+			return nil, ErrUnrecognizedLine
+		}
+
+		var pr PingReply
+
+		replySize, err := strconv.ParseUint(m[1], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		pr.Size = uint(replySize)
+		pr.FromAddress = m[2]
+		replySeqNo, err := strconv.ParseUint(m[3], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		pr.SequenceNumber = uint(replySeqNo)
+		replyTTL, err := strconv.ParseUint(m[4], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		pr.TTL = uint(replyTTL)
+
+		if len(m[5]) != 5 {
+			return nil, ErrUnexpectedFloatFormat
+		}
+
+		// remove dot and leading zeroes
+		s := strings.TrimLeft(strings.Replace(m[5], ".", "", -1), "0")
+		us, err := strconv.ParseUint(s, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		pr.Time = time.Microsecond * time.Duration(us)
+		po.Replies = append(po.Replies, pr)
+	}
+
+	header := lines[last+2]
+	m = statsSeparatorRx.FindStringSubmatch(header)
+	if len(m) < 2 {
+		return nil, ErrMalformedStats
+	}
+	po.Stats.IPAddress = m[1]
+
+	m = statsLine1.FindStringSubmatch(lines[last+3])
+	if len(m) < 5 {
+		return nil, ErrMalformedStats
+	}
+	packetsTransmitted, err := strconv.ParseUint(m[1], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.Stats.PacketsTransmitted = uint(packetsTransmitted)
+
+	packetsReceived, err := strconv.ParseUint(m[2], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.Stats.PacketsReceived = uint(packetsReceived)
+
+	packetLossPcent, err := strconv.ParseUint(m[3], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.Stats.PacketLossPercent = uint8(packetLossPcent)
+
+	t, err := strconv.ParseUint(m[4], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	po.Stats.Time = time.Millisecond * time.Duration(t)
+
+	m = statsLine2.FindStringSubmatch(lines[last+4])
+	fmt.Println(m)
+
+	return nil, nil
+}
--- pinger/parser/parser_test.go
+++ pinger/parser/parser_test.go
@@ -1,12 +1,11 @@
-package pingparse
+package parser
 
 import (
 	"testing"
 )
 
 const (
-	payloadPing1 = `ping -n -v -w 15 -i 5 127.0.0.1
-PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
+	payloadPing1 = `PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
 64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.026 ms
 64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.021 ms
 64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.031 ms
@@ -18,5 +17,10 @@ rtt min/avg/max/mdev = 0.021/0.026/0.031/0.004 ms
 )
 
 func TestSimplePing(t *testing.T) {
+	po, err := Parse(payloadPing1)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	t.Logf("result: %v", po)
 }
