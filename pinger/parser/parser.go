e0cf595024db805eeed80d81dab3ddb93b5eb79d
gm42 <gm42@users.noreply.github.com>
2017-08-31 10:08:39 +0200

    Introduce named groups
    Rewriting testcases
    
    Completed fix of testcases

--- pinger/parser/parser.go
+++ pinger/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -17,20 +18,30 @@ var (
 	ErrMalformedStatsLine2  = errors.New("malformed stats line 2")
 )
 
+type ConversionError struct {
+	Context string
+	Err     error
+}
+
+func (ce ConversionError) Error() string {
+	return fmt.Sprintf("%s: %v", ce.Context, ce.Err)
+}
+
 var (
-	headerRx             = regexp.MustCompile(`^PING (\d+\.\d+\.\d+\.\d+) \((\d+\.\d+\.\d+\.\d+)\) (\d+)\((\d+)\) bytes of data\.$`)
-	lineRx               = regexp.MustCompile(`^(\d+) bytes from (\d+\.\d+\.\d+\.\d+): icmp_seq=(\d+) ttl=(\d+) time=(.*)$`)
-	statsSeparatorRx     = regexp.MustCompile(`^--- (\d+\.\d+\.\d+\.\d+) ping statistics ---$`)
-	statsLine1           = regexp.MustCompile(`^(\d+) packets transmitted, (\d+) received, (\d+)% packet loss, time (.*)$`)
-	statsLine1WithErrors = regexp.MustCompile(`^(\d+) packets transmitted, (\d+) received, \+(\d+) errors, (\d+)% packet loss, time (.*)$`)
-	statsLine2           = regexp.MustCompile(`^rtt min/avg/max/mdev = ([^/]+)/([^/]+)/([^/]+)/([^ ]+) (.*)$`)
-	pipeNo               = regexp.MustCompile(`([^,]+), pipe (\d+)$`)
-	hostErrorLineRx      = regexp.MustCompile(`^From (\d+\.\d+\.\d+\.\d+) icmp_seq=(\d+) (.*)$`)
+	headerRx             = regexp.MustCompile(`^PING (?P<host>\d+\.\d+\.\d+\.\d+) \((?P<resolvedIPAddress>\d+\.\d+\.\d+\.\d+)\) (?P<payloadSize>\d+)\((?P<payloadActualSize>\d+)\) bytes of data`)
+	headerRxAlt          = regexp.MustCompile(`^PING (?P<host>\d+\.\d+\.\d+\.\d+) \((?P<resolvedIPAddress>\d+\.\d+\.\d+\.\d+)\): (?P<payloadSize>\d+) data bytes`)
+	lineRx               = regexp.MustCompile(`^(?P<replySize>\d+) bytes from (?P<fromAddress>\d+\.\d+\.\d+\.\d+): icmp_seq=(?P<seqNo>\d+) ttl=(?P<ttl>\d+) time=(?P<time>.*)$`)
+	statsSeparatorRx     = regexp.MustCompile(`^--- (?P<IPAddress>\d+\.\d+\.\d+\.\d+) ping statistics ---$`)
+	statsLine1           = regexp.MustCompile(`^(?P<packetsTransmitted>\d+) packets transmitted, (?P<packetsReceived>\d+) (packets )?received, (?P<packetLoss>\d+)% packet loss(, time (?P<time>.*))?$`)
+	statsLine1WithErrors = regexp.MustCompile(`^(?P<packetsTransmitted>\d+) packets transmitted, (?P<packetsReceived>\d+) (packets )?received, \+(?P<errors>\d+) errors, (?P<packetLoss>\d+)% packet loss(, time (?P<time>.*))?$`)
+	statsLine2           = regexp.MustCompile(`^(rtt|round-trip) min/avg/max/(mdev|stddev) = (?P<min>[^/]+)/(?P<avg>[^/]+)/(?P<max>[^/]+)/(?P<mdev>[^ ]+) (?P<unit>.*)$`)
+	pipeNo               = regexp.MustCompile(`(?P<unit>[^,]+), pipe (?P<pipeNo>\d+)$`)
+	hostErrorLineRx      = regexp.MustCompile(`^From (?P<fromIPAddress>\d+\.\d+\.\d+\.\d+) icmp_seq=(?P<seqNo>\d+) (?P<error>.*)$`)
 )
 
 // PingOutput contains the whole ping operation output.
 type PingOutput struct {
-	IPAddress         string
+	Host              string
 	ResolvedIPAddress string
 	PayloadSize       uint
 	PayloadActualSize uint
@@ -43,10 +54,10 @@ type PingReply struct {
 	Size           uint
 	FromAddress    string
 	SequenceNumber uint
-	Duplicate      bool
 	TTL            uint
 	Time           time.Duration
 	Error          string
+	Duplicate      bool
 }
 
 // PingStatistics contains the statistics of the whole ping operation.
@@ -57,10 +68,22 @@ type PingStatistics struct {
 	Errors             uint
 	PacketLossPercent  uint8
 	Time               time.Duration
-	RoundTrip          time.Duration
-	Average            time.Duration
-	Max                time.Duration
-	MeanDeviation      time.Duration
+	RoundTripMin       time.Duration
+	RoundTripAverage   time.Duration
+	RoundTripMax       time.Duration
+	RoundTripDeviation time.Duration
+}
+
+func matchAsMap(rx *regexp.Regexp, s string) map[string]string {
+	m := rx.FindStringSubmatch(s)
+	result := make(map[string]string)
+	if len(m) != 0 {
+		for i, name := range rx.SubexpNames()[1:] {
+			result[name] = m[i+1]
+		}
+	}
+
+	return result
 }
 
 // Parse will parse the specified ping output and return all the information in a a PingOutput object.
@@ -73,23 +96,30 @@ func Parse(s string) (*PingOutput, error) {
 		return nil, ErrNotEnoughLines
 	}
 
-	m := headerRx.FindStringSubmatch(lines[0])
-	if len(m) != 5 {
-		return nil, ErrHeaderMismatch
+	result := matchAsMap(headerRx, lines[0])
+	if len(result) == 0 {
+		result = matchAsMap(headerRxAlt, lines[0])
+		if len(result) == 0 {
+			return nil, ErrHeaderMismatch
+		}
 	}
-	po.IPAddress = m[1]
-	po.ResolvedIPAddress = m[2]
-	payloadSize, err := strconv.ParseUint(m[3], 10, 64)
+	po.Host = result["host"]
+	po.ResolvedIPAddress = result["resolvedIPAddress"]
+	payloadSize, err := strconv.ParseUint(result["payloadSize"], 10, 64)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"payloadSize", err}
 	}
 	po.PayloadSize = uint(payloadSize)
-	payloadActualSize, err := strconv.ParseUint(m[4], 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	po.PayloadActualSize = uint(payloadActualSize)
 
+	if v, ok := result["payloadActualSize"]; ok {
+		payloadActualSize, err := strconv.ParseUint(v, 10, 64)
+		if err != nil {
+			return nil, ConversionError{"payloadActualSize", err}
+		}
+		po.PayloadActualSize = uint(payloadActualSize)
+	}
+
+	// start parsing replies
 	var last int
 	for i, line := range lines[1:] {
 		if line == "" {
@@ -104,100 +134,114 @@ func Parse(s string) (*PingOutput, error) {
 			line = line[:len(line)-7]
 		}
 
-		m := lineRx.FindStringSubmatch(line)
-		if len(m) != 6 {
+		result = matchAsMap(lineRx, line)
+		if len(result) == 0 {
 			// try to match a host problem line
-			m = hostErrorLineRx.FindStringSubmatch(line)
-			if len(m) == 4 {
-				pr.FromAddress = m[1]
-				replySeqNo, err := strconv.ParseUint(m[2], 10, 64)
+			result := matchAsMap(hostErrorLineRx, line)
+			if len(result) != 0 {
+				pr.FromAddress = result["fromAddress"]
+				replySeqNo, err := strconv.ParseUint(result["seqNo"], 10, 64)
 				if err != nil {
-					return nil, err
+					return nil, ConversionError{"error reply seqNo", err}
 				}
 				pr.SequenceNumber = uint(replySeqNo)
-				pr.Error = m[3]
+				pr.Error = result["error"]
+
+				// cleanup 'result', as it is checked after the loop
+				result = map[string]string{}
 
 				po.Replies = append(po.Replies, pr)
 				continue
 			}
+
+			// some ping outputs have a new line separator, others don't
+			result = matchAsMap(statsSeparatorRx, line)
+			if len(result) != 0 {
+				last = i + 1
+				break
+			}
+
 			return nil, ErrUnrecognizedLine
 		}
 
-		replySize, err := strconv.ParseUint(m[1], 10, 64)
+		replySize, err := strconv.ParseUint(result["replySize"], 10, 64)
 		if err != nil {
-			return nil, err
+			return nil, ConversionError{"replySize", err}
 		}
 		pr.Size = uint(replySize)
-		pr.FromAddress = m[2]
-		replySeqNo, err := strconv.ParseUint(m[3], 10, 64)
+		pr.FromAddress = result["fromAddress"]
+		replySeqNo, err := strconv.ParseUint(result["seqNo"], 10, 64)
 		if err != nil {
-			return nil, err
+			return nil, ConversionError{"reply seqNo", err}
 		}
 		pr.SequenceNumber = uint(replySeqNo)
-		replyTTL, err := strconv.ParseUint(m[4], 10, 64)
+		replyTTL, err := strconv.ParseUint(result["ttl"], 10, 64)
 		if err != nil {
-			return nil, err
+			return nil, ConversionError{"ttl", err}
 		}
 		pr.TTL = uint(replyTTL)
 
-		pr.Time, err = time.ParseDuration(strings.Replace(m[5], " ", "", -1))
+		pr.Time, err = time.ParseDuration(strings.Replace(result["time"], " ", "", -1))
 		if err != nil {
-			return nil, err
+			return nil, ConversionError{"ping reply time", err}
 		}
+
+		// cleanup 'result', as it is checked after the loop
+		result = map[string]string{}
 
 		po.Replies = append(po.Replies, pr)
 	}
 
-	// parse header
-	m = statsSeparatorRx.FindStringSubmatch(lines[last])
-	if len(m) != 2 {
-		return nil, ErrMalformedStatsHeader
+	if len(result) == 0 {
+		// parse header
+		result = matchAsMap(statsSeparatorRx, lines[last])
+		if len(result) == 0 {
+			return nil, ErrMalformedStatsHeader
+		}
 	}
-	po.Stats.IPAddress = m[1]
+	po.Stats.IPAddress = result["IPAddress"]
 
 	// parse stats line 1
 	last++
-	m = statsLine1.FindStringSubmatch(lines[last])
-	var idx int
-	var hasError bool
-	if len(m) != 5 {
+	result = matchAsMap(statsLine1, lines[last])
+	if len(result) == 0 {
 		// check if it's a line with errors
-		m = statsLine1WithErrors.FindStringSubmatch(lines[last])
-		if len(m) != 6 {
+		result = matchAsMap(statsLine1WithErrors, lines[last])
+		if len(result) == 0 {
 			return nil, ErrMalformedStatsLine1
 		}
-		hasError = true
-		idx = 1
 	}
-	packetsTransmitted, err := strconv.ParseUint(m[1], 10, 64)
+	packetsTransmitted, err := strconv.ParseUint(result["packetsTransmitted"], 10, 64)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"packetsTransmitted", err}
 	}
 	po.Stats.PacketsTransmitted = uint(packetsTransmitted)
 
-	packetsReceived, err := strconv.ParseUint(m[2], 10, 64)
+	packetsReceived, err := strconv.ParseUint(result["packetsReceived"], 10, 64)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"packetsReceived", err}
 	}
 	po.Stats.PacketsReceived = uint(packetsReceived)
 
-	if hasError {
-		errCount, err := strconv.ParseUint(m[3], 10, 64)
+	if v, ok := result["errors"]; ok {
+		errCount, err := strconv.ParseUint(v, 10, 64)
 		if err != nil {
-			return nil, err
+			return nil, ConversionError{"errors", err}
 		}
 		po.Stats.Errors = uint(errCount)
 	}
 
-	packetLossPcent, err := strconv.ParseUint(m[3+idx], 10, 64)
+	packetLossPcent, err := strconv.ParseUint(result["packetLoss"], 10, 64)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"packetLoss", err}
 	}
 	po.Stats.PacketLossPercent = uint8(packetLossPcent)
 
-	po.Stats.Time, err = time.ParseDuration(m[4+idx])
-	if err != nil {
-		return nil, err
+	if v, ok := result["time"]; ok && len(v) != 0 {
+		po.Stats.Time, err = time.ParseDuration(v)
+		if err != nil {
+			return nil, ConversionError{"stats time", err}
+		}
 	}
 
 	validReplies := 0
@@ -208,40 +252,39 @@ func Parse(s string) (*PingOutput, error) {
 		}
 		validReplies++
 	}
-
 	if validReplies == 0 {
 		return &po, nil
 	}
 
 	// parse stats line 2
 	last++
-	m = statsLine2.FindStringSubmatch(lines[last])
-	if len(m) != 6 {
+	result = matchAsMap(statsLine2, lines[last])
+	if len(result) == 0 {
 		return nil, ErrMalformedStatsLine2
 	}
 
-	unit := m[5]
-	pm := pipeNo.FindStringSubmatch(unit)
-	if len(pm) > 1 {
-		unit = pm[1]
+	unit := result["unit"]
+	pm := matchAsMap(pipeNo, unit)
+	if len(pm) != 0 {
+		unit = pm["unit"]
 		// pipe number in pm[2] is ignored
 	}
 
-	po.Stats.RoundTrip, err = time.ParseDuration(m[1] + unit)
+	po.Stats.RoundTripMin, err = time.ParseDuration(result["min"] + unit)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"rtt", err}
 	}
-	po.Stats.Average, err = time.ParseDuration(m[2] + unit)
+	po.Stats.RoundTripAverage, err = time.ParseDuration(result["avg"] + unit)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"avg", err}
 	}
-	po.Stats.Max, err = time.ParseDuration(m[3] + unit)
+	po.Stats.RoundTripMax, err = time.ParseDuration(result["max"] + unit)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"max", err}
 	}
-	po.Stats.MeanDeviation, err = time.ParseDuration(m[4] + unit)
+	po.Stats.RoundTripDeviation, err = time.ParseDuration(result["mdev"] + unit)
 	if err != nil {
-		return nil, err
+		return nil, ConversionError{"mdev", err}
 	}
 
 	return &po, nil
--- pinger/parser/parser_test.go
+++ pinger/parser/parser_test.go
@@ -1,12 +1,156 @@
 package parser
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
-const (
-	payload1 = `PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
+var (
+	expectedTestCases = []PingOutput{
+		// 0
+		PingOutput{
+			Host:              `127.0.0.1`,
+			ResolvedIPAddress: `127.0.0.1`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Replies: []PingReply{
+				PingReply{64, `127.0.0.1`, 1, 64, 26 * time.Microsecond, "", false},
+				PingReply{64, `127.0.0.1`, 2, 64, 21 * time.Microsecond, "", false},
+				PingReply{64, `127.0.0.1`, 3, 64, 31 * time.Microsecond, "", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `127.0.0.1`,
+				PacketsTransmitted: 3,
+				PacketsReceived:    3,
+				Time:               10021 * time.Millisecond,
+				RoundTripMin:       21 * time.Microsecond,
+				RoundTripMax:       31 * time.Microsecond,
+				RoundTripAverage:   26 * time.Microsecond,
+				RoundTripDeviation: 4 * time.Microsecond,
+			},
+		},
+		// 1
+		PingOutput{
+			Host:              `172.17.0.1`,
+			ResolvedIPAddress: `172.17.0.1`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Replies: []PingReply{
+				PingReply{64, `172.17.0.1`, 1, 64, 98 * time.Microsecond, "", false},
+				PingReply{64, `172.17.0.1`, 2, 64, 90 * time.Microsecond, "", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `172.17.0.1`,
+				PacketsTransmitted: 3,
+				PacketsReceived:    2,
+				PacketLossPercent:  33,
+				Time:               10292 * time.Millisecond,
+				RoundTripMin:       90 * time.Microsecond,
+				RoundTripMax:       98 * time.Microsecond,
+				RoundTripAverage:   94 * time.Microsecond,
+				RoundTripDeviation: 4 * time.Microsecond,
+			},
+		},
+		// 2
+		PingOutput{
+			Host:              `172.17.0.1`,
+			ResolvedIPAddress: `172.17.0.1`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Stats: PingStatistics{
+				PacketsTransmitted: 3,
+				PacketsReceived:    0,
+				PacketLossPercent:  100,
+				Time:               10205 * time.Millisecond,
+			},
+		},
+		// 3
+		PingOutput{
+			Host:              `172.17.0.2`,
+			ResolvedIPAddress: `172.17.0.2`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Replies: []PingReply{
+				PingReply{64, `172.17.0.2`, 4, 63, 286 * time.Millisecond, "", false},
+				PingReply{64, `172.17.0.2`, 5, 63, 111 * time.Millisecond, "", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `172.17.0.2`,
+				PacketsTransmitted: 5,
+				PacketsReceived:    2,
+				PacketLossPercent:  60,
+				Time:               4055 * time.Millisecond,
+				RoundTripMin:       111409 * time.Microsecond,
+				RoundTripMax:       286063 * time.Microsecond,
+				RoundTripAverage:   198736 * time.Microsecond,
+				RoundTripDeviation: 87327 * time.Microsecond,
+			},
+		},
+		// 4
+		PingOutput{
+			Host:              `172.17.0.2`,
+			ResolvedIPAddress: `172.17.0.2`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Replies: []PingReply{
+				PingReply{64, `172.17.0.2`, 4, 63, 286 * time.Millisecond, "", false},
+				PingReply{64, `172.17.0.2`, 5, 63, 111 * time.Millisecond, "", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `172.17.0.2`,
+				PacketsTransmitted: 5,
+				PacketsReceived:    2,
+				PacketLossPercent:  60,
+				Time:               4055 * time.Millisecond,
+				RoundTripMin:       111409 * time.Microsecond,
+				RoundTripMax:       286063 * time.Microsecond,
+				RoundTripAverage:   198736 * time.Microsecond,
+				RoundTripDeviation: 87327 * time.Microsecond,
+			},
+		},
+		// 5
+		PingOutput{
+			Host:              `172.17.0.3`,
+			ResolvedIPAddress: `172.17.0.3`,
+			PayloadSize:       56,
+			PayloadActualSize: 84,
+			Replies: []PingReply{
+				PingReply{0, ``, 2, 0, 0, "Destination Host Unreachable", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `172.17.0.3`,
+				Errors:             1,
+				PacketsTransmitted: 4,
+				PacketsReceived:    0,
+				PacketLossPercent:  100,
+				Time:               3055 * time.Millisecond,
+			},
+		},
+		// 6
+		PingOutput{
+			Host:              `127.0.0.1`,
+			ResolvedIPAddress: `127.0.0.1`,
+			PayloadSize:       56,
+			Replies: []PingReply{
+				PingReply{64, `127.0.0.1`, 0, 64, 61 * time.Microsecond, "", false},
+				PingReply{64, `127.0.0.1`, 1, 64, 57 * time.Microsecond, "", false},
+				PingReply{64, `127.0.0.1`, 2, 64, 108 * time.Microsecond, "", false},
+			},
+			Stats: PingStatistics{
+				IPAddress:          `127.0.0.1`,
+				PacketsTransmitted: 3,
+				PacketsReceived:    3,
+				RoundTripMin:       57 * time.Microsecond,
+				RoundTripMax:       108 * time.Microsecond,
+				RoundTripAverage:   75 * time.Microsecond,
+				RoundTripDeviation: 23 * time.Microsecond,
+			},
+		},
+	}
+	payloads = []string{
+		// 0
+		`PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
 64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.026 ms
 64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.021 ms
 64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.031 ms
@@ -14,186 +158,162 @@ const (
 --- 127.0.0.1 ping statistics ---
 3 packets transmitted, 3 received, 0% packet loss, time 10021ms
 rtt min/avg/max/mdev = 0.021/0.026/0.031/0.004 ms
-`
-	payload2 = `PING 172.17.0.1 (172.17.0.1) 56(84) bytes of data.
+`,
+		// 1
+		`PING 172.17.0.1 (172.17.0.1) 56(84) bytes of data.
 64 bytes from 172.17.0.1: icmp_seq=1 ttl=64 time=0.098 ms
 64 bytes from 172.17.0.1: icmp_seq=2 ttl=64 time=0.090 ms
 
 --- 172.17.0.1 ping statistics ---
 3 packets transmitted, 2 received, 33% packet loss, time 10292ms
 rtt min/avg/max/mdev = 0.090/0.094/0.098/0.004 ms
-`
-	payload3 = `PING 172.17.0.1 (172.17.0.1) 56(84) bytes of data.
+`,
+		// 2
+		`PING 172.17.0.1 (172.17.0.1) 56(84) bytes of data.
 
 --- 172.17.0.1 ping statistics ---
 3 packets transmitted, 0 received, 100% packet loss, time 10205ms
-`
-	payload4 = `PING 172.17.0.2 (172.17.0.2) 56(84) bytes of data.
+`,
+		// 3
+		`PING 172.17.0.2 (172.17.0.2) 56(84) bytes of data.
 64 bytes from 172.17.0.2: icmp_seq=4 ttl=63 time=286 ms
 64 bytes from 172.17.0.2: icmp_seq=5 ttl=63 time=111 ms
 
 --- 172.17.0.2 ping statistics ---
 5 packets transmitted, 2 received, 60% packet loss, time 4055ms
 rtt min/avg/max/mdev = 111.409/198.736/286.063/87.327 ms
-`
-	payload5 = `PING 172.17.0.2 (172.17.0.2) 56(84) bytes of data.
+`,
+		// 4
+		`PING 172.17.0.2 (172.17.0.2) 56(84) bytes of data.
 64 bytes from 172.17.0.2: icmp_seq=4 ttl=63 time=286 ms
 64 bytes from 172.17.0.2: icmp_seq=5 ttl=63 time=111 ms
 
 --- 172.17.0.2 ping statistics ---
 5 packets transmitted, 2 received, 60% packet loss, time 4055ms
 rtt min/avg/max/mdev = 111.409/198.736/286.063/87.327 ms, pipe 2
-`
-	payload6 = `PING 172.17.0.3 (172.17.0.3) 56(84) bytes of data.
+`,
+		// 5
+		`PING 172.17.0.3 (172.17.0.3) 56(84) bytes of data.
 From 93.184.216.34 icmp_seq=2 Destination Host Unreachable
 
 --- 172.17.0.3 ping statistics ---
 4 packets transmitted, 0 received, +1 errors, 100% packet loss, time 3055ms
 pipe 3
-`
+`,
+		// 6
+		`PING 127.0.0.1 (127.0.0.1): 56 data bytes, id 0x0001 = 1
+64 bytes from 127.0.0.1: icmp_seq=0 ttl=64 time=0.061 ms
+64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.057 ms
+64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.108 ms
+--- 127.0.0.1 ping statistics ---
+3 packets transmitted, 3 packets received, 0% packet loss
+round-trip min/avg/max/stddev = 0.057/0.075/0.108/0.023 ms
+`}
 )
 
-func TestSimplePing(t *testing.T) {
-	po, err := Parse(payload1)
-	if err != nil {
-		t.Fatal(err)
+func TestPings(t *testing.T) {
+	for i := 0; i < len(payloads); i++ {
+		// capture range variables
+		payload := payloads[i]
+		expected := expectedTestCases[i]
+		t.Run(fmt.Sprintf("payload #%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			po, err := Parse(payload)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if po.Host != expected.Host {
+				t.Errorf("expected host %q, but got %q", expected.Host, po.Host)
+			}
+
+			if po.ResolvedIPAddress != expected.ResolvedIPAddress {
+				t.Errorf("expected resolved IP address %q, but got %q", expected.ResolvedIPAddress, po.ResolvedIPAddress)
+			}
+
+			if po.PayloadSize != expected.PayloadSize {
+				t.Errorf("expected payload size %v, but got %v", expected.PayloadSize, po.PayloadSize)
+			}
+			if po.PayloadActualSize != expected.PayloadActualSize {
+				t.Errorf("expected payload actual size %v, but got %v", expected.PayloadActualSize, po.PayloadActualSize)
+			}
+
+			///
+			/// check stats fields
+			///
+
+			if po.Stats.IPAddress != expected.Stats.IPAddress {
+				t.Errorf("expected stats IP address %q, but got %q", expected.Stats.IPAddress, po.Stats.IPAddress)
+			}
+
+			if po.Stats.Errors != expected.Stats.Errors {
+				t.Errorf("expected errors %v, but got %v", expected.Stats.Errors, po.Stats.Errors)
+			}
+			if po.Stats.PacketLossPercent != expected.Stats.PacketLossPercent {
+				t.Errorf("expected packet loss percent %v, but got %v", expected.Stats.PacketLossPercent, po.Stats.PacketLossPercent)
+			}
+			if po.Stats.Time != expected.Stats.Time {
+				t.Errorf("expected time %v, but got %v", expected.Stats.Time, po.Stats.Time)
+			}
+
+			if po.Stats.RoundTripMin != expected.Stats.RoundTripMin {
+				t.Errorf("expected rtt min %v, but got %v", expected.Stats.RoundTripMin, po.Stats.RoundTripMin)
+			}
+			if po.Stats.RoundTripMax != expected.Stats.RoundTripMax {
+				t.Errorf("expected rtt max %v, but got %v", expected.Stats.RoundTripMax, po.Stats.RoundTripMax)
+			}
+			if po.Stats.RoundTripAverage != expected.Stats.RoundTripAverage {
+				t.Errorf("expected rtt avg %v, but got %v", expected.Stats.RoundTripAverage, po.Stats.RoundTripAverage)
+			}
+			if po.Stats.RoundTripDeviation != expected.Stats.RoundTripDeviation {
+				t.Errorf("expected rtt mdev %v, but got %v", expected.Stats.RoundTripDeviation, po.Stats.RoundTripDeviation)
+			}
+
+			if po.Stats.PacketsReceived != expected.Stats.PacketsReceived {
+				t.Errorf("expected packets received %v, but got %v", expected.Stats.PacketsReceived, po.Stats.PacketsReceived)
+			}
+			if po.Stats.PacketsTransmitted != expected.Stats.PacketsTransmitted {
+				t.Errorf("expected packets transmitted %v, but got %v", expected.Stats.PacketsTransmitted, po.Stats.PacketsTransmitted)
+			}
+
+			if len(expected.Replies) != len(po.Replies) {
+				t.Errorf("expected %d replies, but got %d", len(expected.Replies), len(po.Replies))
+			}
+
+			for i := 0; i < len(po.Replies); i++ {
+				pr := po.Replies[i]
+				epr := expected.Replies[i]
+				if epr.SequenceNumber != pr.SequenceNumber {
+					t.Errorf("reply %d: expected sequence number %d, but got %d", i, epr.SequenceNumber, pr.SequenceNumber)
+				}
+
+				if epr.Time != pr.Time {
+					t.Errorf("reply %d: expected time %v, but got %v", i, epr.Time, pr.Time)
+				}
+
+				if epr.Size != pr.Size {
+					t.Errorf("reply %d: expected size %v, but got %v", i, epr.Size, pr.Size)
+				}
+
+				if epr.FromAddress != pr.FromAddress {
+					t.Errorf("reply %d: expected from address %v, but got %v", i, epr.FromAddress, pr.FromAddress)
+				}
+				if epr.SequenceNumber != pr.SequenceNumber {
+					t.Errorf("reply %d: expected sequence number %v, but got %v", i, epr.SequenceNumber, pr.SequenceNumber)
+				}
+				if epr.TTL != pr.TTL {
+					t.Errorf("reply %d: expected TTL %v, but got %v", i, epr.TTL, pr.TTL)
+				}
+				if epr.Error != pr.Error {
+					t.Errorf("reply %d: expected error %q, but got %q", i, epr.Error, pr.Error)
+				}
+				if epr.Duplicate != pr.Duplicate {
+					t.Errorf("reply %d: expected duplicate %v, but got %v", i, epr.Duplicate, pr.Duplicate)
+				}
+			}
+
+		})
 	}
 
-	if len(po.Replies) != 3 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	if po.Replies[1].SequenceNumber != 2 {
-		t.Error("invalid sequence number found")
-	}
-
-	if po.Replies[2].Time != time.Duration(31)*time.Microsecond {
-		t.Error("invalid time of reply 2 found")
-	}
-
-	// check some fields
-	if po.Stats.RoundTrip != time.Duration(21)*time.Microsecond {
-		t.Error("invalid RTT found")
-	}
-	if po.Stats.Average != time.Duration(26)*time.Microsecond {
-		t.Error("invalid avg found")
-	}
-	if po.Stats.Max != time.Duration(31)*time.Microsecond {
-		t.Error("invalid max found")
-	}
-	if po.Stats.MeanDeviation != time.Duration(4)*time.Microsecond {
-		t.Error("invalid max found")
-	}
-}
-
-func TestShakyPing(t *testing.T) {
-	po, err := Parse(payload2)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(po.Replies) != 2 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	if po.Replies[1].SequenceNumber != 2 {
-		t.Error("invalid sequence number found")
-	}
-
-	if po.Replies[0].Time != time.Duration(98)*time.Microsecond {
-		t.Error("invalid time of reply 2 found")
-	}
-
-	// check some fields
-	if po.Stats.RoundTrip != time.Duration(90)*time.Microsecond {
-		t.Error("invalid RTT found")
-	}
-	if po.Stats.Average != time.Duration(94)*time.Microsecond {
-		t.Error("invalid avg found")
-	}
-	if po.Stats.Max != time.Duration(98)*time.Microsecond {
-		t.Error("invalid max found")
-	}
-	if po.Stats.MeanDeviation != time.Duration(4)*time.Microsecond {
-		t.Error("invalid max found")
-	}
-}
-
-func TestFailedPing(t *testing.T) {
-	po, err := Parse(payload3)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(po.Replies) != 0 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	// check some fields
-	if po.Stats.PacketLossPercent != 100 {
-		t.Error("invalid packet loss percentage found")
-	}
-	if po.Stats.PacketsTransmitted != 3 {
-		t.Error("invalid packets transmitted found")
-	}
-	if po.Stats.Time != time.Duration(10205)*time.Millisecond {
-		t.Error("invalid stats time found")
-	}
-}
-
-func TestPingDifferent(t *testing.T) {
-	po, err := Parse(payload4)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(po.Replies) != 2 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	if po.Replies[1].SequenceNumber != 5 {
-		t.Error("invalid sequence number found")
-	}
-
-	if po.Replies[0].Time != time.Duration(286)*time.Millisecond {
-		t.Error("invalid time of reply found")
-	}
-}
-
-func TestPingWithPipe(t *testing.T) {
-	po, err := Parse(payload5)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(po.Replies) != 2 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	if po.Replies[1].SequenceNumber != 5 {
-		t.Error("invalid sequence number found")
-	}
-
-	if po.Replies[0].Time != time.Duration(286)*time.Millisecond {
-		t.Error("invalid time of reply found")
-	}
-}
-
-func TestPingUnreachable(t *testing.T) {
-	po, err := Parse(payload6)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(po.Replies) != 1 {
-		t.Fatal("invalid number of replies found")
-	}
-
-	if po.Replies[0].SequenceNumber != 2 {
-		t.Error("invalid sequence number found")
-	}
-
-	if po.Stats.Errors != 1 {
-		t.Error("invalid errors found")
-	}
 }
